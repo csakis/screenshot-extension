b2e08630705e427a9be93e6a96d7971f12638cc8
Csaba Kiss <csakis@gmail.com>
2019-12-10 16:22:59 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <Container>
+      <h1>It's working</h1>
+      <Button variant="contained" color="primary">
+        Button
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
